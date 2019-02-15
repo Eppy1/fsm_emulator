2d69d445410bb463f76669b3171337bf8bab780b
YegorAlexandrow <yegor99@live.com>
2019-02-15 22:20:45 +0300

    bomj

--- auth.js
+++ auth.js
@@ -1,35 +1,22 @@
-
-function auth_checkAllFilled() {
-    var fields = $('#auth').serializeArray();
-    var result = 'ok'
-
-    jQuery.each( fields, function( i, field ) {
-        if(field.value.length == 0) {
-            result = 'no'
-        }
-    });
-
-    return result == 'ok';
-}
-
-$('#auth').submit(function() {
-    if(!auth_checkAllFilled())  {
-        document.getElementById('auth_alert_txt').innerHTML = "Все поля должны быть заполнены!";
-        return false;
-    }
-
-    e.preventDefault();
-    $.ajax({
-      url: "sendOrderForm.php",
-      type: "POST",
-      data: $('#osForm').serialize(),
-      success: function(response) {
-        //обработка успешной отправки
-      },
-      error: function(response) {
-        //обработка ошибок при отправке
-     }
-    });
-
-	return true;
+$( document ).ready(function() {
+    $("#btn").click(
+		function(){
+			sendAjaxForm('auth', 'auth.php');
+			return false; 
+		}
+	);
 });
+ 
+function sendAjaxForm(ajax_form, url) {
+    jQuery.ajax({
+        url:     url, //url страницы (action_ajax_form.php)
+        type:     "POST", //метод отправки
+        dataType: "html", //формат данных
+        data: jQuery("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        success: function(response) { //Данные отправлены успешно
+        	result = jQuery.parseJSON(response);
+    	},
+    	error: function(response) {
+    	}
+ 	});
+}
